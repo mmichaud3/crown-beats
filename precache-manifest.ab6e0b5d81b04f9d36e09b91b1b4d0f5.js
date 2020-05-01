407e357f9ae1c96c6dbda309a9fefbbcbd148aad
Michael Michaud <mmichaud3@yahoo.com>
2020-04-30 18:03:25 -0700

    Updates

---
+++ precache-manifest.ab6e0b5d81b04f9d36e09b91b1b4d0f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c047394eb2a40733f7bac463beff62",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "1283423a709f8ca36a4e",
+    "url": "/jammming/static/css/main.9a6413a7.chunk.css"
+  },
+  {
+    "revision": "48c672a08f2f8fdd40f6",
+    "url": "/jammming/static/js/2.f0cfd5db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.f0cfd5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1283423a709f8ca36a4e",
+    "url": "/jammming/static/js/main.e2a5adb5.chunk.js"
+  },
+  {
+    "revision": "8982e6a878b4fdae3e5f",
+    "url": "/jammming/static/js/runtime-main.364f2a86.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
