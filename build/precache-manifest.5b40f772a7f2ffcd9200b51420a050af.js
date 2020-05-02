d2a41cc20b1fec953a0c93f4958964784a91ca3f
Michael Michaud <mmichaud3@yahoo.com>
2020-05-02 16:40:36 -0700

    final

---
+++ build/precache-manifest.5b40f772a7f2ffcd9200b51420a050af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63795387c2d52437c6daee1ce9835d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4f5e2cde09f489517f",
+    "url": "/static/css/main.159e3424.chunk.css"
+  },
+  {
+    "revision": "d52f2ed3b95106549720",
+    "url": "/static/js/2.f619cd02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f619cd02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4f5e2cde09f489517f",
+    "url": "/static/js/main.5663addd.chunk.js"
+  },
+  {
+    "revision": "7b2a84bdefa916defe51",
+    "url": "/static/js/runtime-main.300737e4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/original (3).ed28accb.svg"
+  }
+]);
